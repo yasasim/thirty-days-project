57965f9d6e10b5e6f4d0d1aef61e0c6eb00b39d0
yasasim <shimirin23@gmail.com>
2021-02-02 23:52:55 +0900

    canvasを使えるように

--- product/js/index.js
+++ product/js/index.js
@@ -1,5 +1,21 @@
 "use strict";
-const helloWorld = () => {
-    console.log("Hello, World!");
+const font = {
+    message: 20
 };
-helloWorld();
+const main = () => {
+    console.log("Hello, World!");
+    const canvas = document.getElementById("main");
+    if (!canvas.getContext) {
+        alert("canvas is not found");
+        throw new Error("canvas is not found");
+    }
+    const context = canvas.getContext("2d");
+    if (!context) {
+        alert("cannot get context");
+        throw new Error("cannot get context");
+    }
+    context.font = font.message.toString() + "px sans-serif";
+    console.log(context.font);
+    context.fillText("Hello, World!", 0, 24);
+};
+window.onload = main;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-const helloWorld = () => {
+const font = {
+  message: 20
+};
+
+const main = () => {
   console.log("Hello, World!");
+  const canvas = <HTMLCanvasElement>document.getElementById("main");
+  if(!canvas.getContext){
+    alert("canvas is not found");
+    throw new Error("canvas is not found");
+  }
+  const context = canvas.getContext("2d");
+  if(!context){
+    alert("cannot get context");
+    throw new Error("cannot get context");
+  }
+  context.font = font.message.toString() + "px sans-serif";
+  console.log(context.font);
+  context.fillText("Hello, World!", 0, 24);
 }
 
-helloWorld();
+window.onload = main;
